4a19b6a111e48f9a97c0e95e92b51b89447cec18
Linus <linusfrotscher@gmail.com>
2019-10-30 10:55:48 +0100

    update

--- ellipticCurveGroupOperations.py
+++ ellipticCurveGroupOperations.py
@@ -45,7 +45,7 @@ class ellipticCurve:
 
 
 class pointAddition:
-    def __init__(self, P: (int, int), Q: (int, int)):
+    def __init__(self, P, Q):
         self.P = P
         self.Q = Q
     gradient: int
@@ -65,15 +65,61 @@ class pointAddition:
         print(f"\n{self.P} + {self.Q} is ({x}, {y})!")
 
 
-if len(args) == 3:
+class pointDuplication:
+    def __init__(self, P):
+        self.P = P
+    gradient: int
+
+    def calculate(self):
+        divident = (3*(self.P[0]**2) + curve.a) % curve.p
+        diviser = 2 * self.P[1] % curve.p
+        inverse = extendedEuclidian.getInverse(curve.p, diviser)
+        gradient = divident*inverse % curve.p
+        x = (gradient ** 2 - self.P[0] - self.P[0]) % curve.p
+        y = turnPositive(
+            curve.p, ((gradient*(self.P[0]-x)-self.P[1]) % curve.p))
+        print(f"\n{self.P} + {self.P} is ({x}, {y})!")
+
+
+def pointExists(x, y):
+    givenPoint = (x, y)
+    isPoint = False
+    for point in curve.points:
+        if givenPoint == point:
+            isPoint = True
+    return isPoint
+
+
+if 3 <= len(args) <= 7:
     try:
         curve = ellipticCurve(int(args[0]), int(args[1]), int(args[2]))
         curve.getInfo()
+        if len(args) == 4:
+            print("\na point needs a x and a y value")
+        elif len(args) == 5:
+            x = int(args[3])
+            y = int(args[4])
+            if pointExists(x, y):
+                duplication = pointDuplication((x, y))
+                duplication.calculate()
+            else:
+                print("the given point is not an element of the group")
+        elif len(args) == 6:
+            print("\na point needs a x and a y value")
+        elif len(args) == 7:
+            x1 = int(args[3])
+            y1 = int(args[4])
+            x2 = int(args[5])
+            y2 = int(args[6])
+            if pointExists(x1, y1) and pointExists(x2, y2):
+                addition = pointAddition((x1, y1), (x2, y2))
+                addition.calculate()
+            else:
+                print("one of the given points is not an element of the group")
+
     except:
         print("\nthe arguments need to be integers! try again...")
 
 else:
     print("\nyou need to enter a modulus and both a and b for the curve\nhere is an example\npy ellipticCurveGroupOperations.py p a b")
-
-addition = pointAddition((1, 2), (9, 12))
-addition.calculate()
+    print("\nyou can also enter two or four more arguments to perform point duplication or addition with them")
